72d2f22dd04a0c48c36743dd5a5d780f673e720b
VIKTORIAIT <89775075+VIKTORIAIT@users.noreply.github.com>
2021-11-15 22:47:14 +0200

    Add files

---
+++ src/index.js
@@ -0,0 +1,64 @@
+import './css/styles.css';
+import fetchCountries from './fetchCountries.js';
+import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+const DEBOUNCE_DELAY = 300;
+
+const inputEl = document.querySelector('#search-box');
+
+inputEl.addEventListener('input', debounce(searchFunc, DEBOUNCE_DELAY));
+
+function searchFunc(event) {
+  const valueEl = event.target.value.trim();
+  fetchCountries(valueEl)
+    .then(data => {
+      if (data.status === 404) {
+        removeElems();
+        throw new Error(error);
+      }
+      let count = data.length;
+      if (count > 10) {
+        removeElems();
+        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+      } else if (count >= 2 && count <= 10) {
+        removeElems();
+        renderListCountries(data);
+      } else if (count === 1) {
+        renderSingleCountry(data);
+      }
+    })
+    .catch(error => {
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+    });
+}
+
+function renderListCountries(dataCountries) {
+  countryInfo.innerHTML = '';
+  let country = dataCountries
+    .map(({ name: { official }, flags: { svg } }) => {
+      return `<li><div class="divstyle"><img width="70px" src="${svg}"/><b>${official}</b></div></li>`;
+    })
+    .join('');
+  countryList.innerHTML = country;
+}
+
+function renderSingleCountry(dataCountries) {
+  countryList.innerHTML = '';
+  let country = dataCountries
+    .map(({ name: { official }, flags: { svg }, capital, population, languages }) => {
+      const langArray = Object.values(languages).join('');
+      return `<div class="divstyle"><img width=70px src="${svg}"/><p class="countryNameText">${official}</p></div>
+      <p><span>Capital: </span>${capital}</p>
+      <p><span>Population: </span>${population}</p>
+      <p><span>Languages: </span>${langArray}</p>`;
+    })
+    .join('');
+  countryInfo.innerHTML = country;
+}
+
+function removeElems() {
+  countryInfo.innerHTML = '';
+  countryList.innerHTML = '';
+}
